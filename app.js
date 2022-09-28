03a8be35469bb4fd1f71aecd42d0c84e28dcdb70
eyupGuc <meydanguresi@gmail.com>
2022-09-28 06:59:02 +0300

    designed

--- app.js
+++ app.js
@@ -1,42 +1,16 @@
-const fetchApiCountry = () => {
-  const url = `https://restcountries.com/v3.1/all`;
-  fetch(url)
-    .then((res) => {
-      if (!res.ok) {
-        renderError(`Something went wrong: ${res.status}`);
-        throw new Error();
-      } else {
-        return res.json();
-      }
-    })
-    .then((data) => renderCountries(data))
-    .catch((e) => console.log(e));
-};
-const renderError = () => {
-  const countriesDiv = document.querySelector(".countries");
-  countriesDiv.innerHTML += `<h2>Country not found <img src="./img/404.jpg"/>`;
+const fetchCountries = async () => {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  const data = await res.json();
+  console.log(data);
+  box(data);
 };
 
-const renderCountries = (data) => {
-  console.log(data);
+const box = (data) => {
   const selectDiv = document.querySelector(".selectDiv");
   console.log(selectDiv);
-  data.forEach((country) => {
-    selectDiv.innerHTML += `<option value="${country.name.common}">${country.name.common}</option>`;
+  data.forEach((item) => {
+    selectDiv.innerHTML += `<option value="${item.name.common}">${item.name.common}</option>`;
   });
 };
-selectDiv.addEventListener("change", (e) => {
-  updateDom(e.target.value, data);
-});
 
-fetchApiCountry();
-const updateDom = (value, data) => {
-  console.log(data[0].name.common);
-
-  let country = data.filter((item) => {
-    if (item.name.common == value) {
-      return item.name.common;
-    }
-  });
-  console.log(country);
-};
+window.addEventListener("load", fetchCountries());
